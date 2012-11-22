5fafdc11e6ae72844fb828a2690c0996c308ce82
Max <max@ubuntu.(none)>
2012-11-22 21:44:55 +0100

    Développement du soir

--- src/xv25.cc
+++ src/xv25.cc
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <sstream>
+#include <errno.h>
 #include "xv25.hh"
+#include <string.h>
+#include "serial.h"
+
 
 XV25::XV25(string portName)
 {
@@ -16,46 +20,11 @@ XV25::~XV25()
 status_t XV25::connect()
 {
     status_t ret = STATUS_OK;
-    struct termios toptions;
 
-    port = open(portName.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
+    port = serial_open(portName.c_str(), B9600);
     if (port <= 0) {
         cerr << "Failed to open \"" << portName << "\"" << endl;
         ret = STATUS_ERROR;
-    }
-
-    if (STATUS_OK == ret) {
-        if (tcgetattr(port, &toptions) < 0) {
-            cerr << "Couldn't get term attributes" << endl;
-            ret = STATUS_ERROR;
-        } else {
-            speed_t brate = B9600;
-            cfsetispeed(&toptions, brate);
-            cfsetospeed(&toptions, brate);
-
-            // 8N1
-            toptions.c_cflag &= ~PARENB;
-            toptions.c_cflag &= ~CSTOPB;
-            toptions.c_cflag &= ~CSIZE;
-            toptions.c_cflag |= CS8;
-            // no flow control
-            toptions.c_cflag &= ~CRTSCTS;
-
-            toptions.c_cflag |= CREAD | CLOCAL;  // turn on READ & ignore ctrl lines
-            toptions.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
-
-            toptions.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
-            toptions.c_oflag &= ~OPOST; // make raw
-
-            // see: http://unixwiz.net/techtips/termios-vmin-vtime.html
-            toptions.c_cc[VMIN]  = 0;
-            toptions.c_cc[VTIME] = 20;
-    
-            if (tcsetattr(port, TCSANOW, &toptions) < 0) {
-                cerr << "Couldn't set term attributes" << endl;
-                ret = STATUS_ERROR;
-            }
-        }
     }
 
     return ret;
@@ -71,12 +40,12 @@ void XV25::disconnect()
 status_t XV25::send(string cmd)
 {
     status_t ret = STATUS_OK;
-    int size = cmd.size();
 
-    if (0 >= port || size == write(port, cmd.c_str(), size)) {
-        ret = STATUS_ERROR;
-        cerr << "Failed to write \"" << cmd << "\" to \"" << portName << "\"" << endl;
-    }
+    cerr << "Sending command \"" << (cmd.substr(0, cmd.size()-1)) << "\"" << endl;
+
+    if (port > 0)
+        for (uint8_t i = 0; i < cmd.size(); i++)
+            serial_send(port, cmd[i]);
 
     return ret;
 }
@@ -84,23 +53,30 @@ status_t XV25::send(string cmd)
 string XV25::receive(void)
 {
     string response;
-    char b[1];
-    int n;
+    uint8_t byte;
+
+    do {
+        byte = serial_recv(port);
+    } while (0 == byte);
+    response += (char)byte;
+
+    cerr << "read " << byte << endl;
 
     if (0 < port) {
-        do { 
-            n = read(port, b, 1);
-            if (-1 == n) {
-                cerr << "Failed to read from \"" << portName << "\", already got \"" << response << "\"" << endl;
-                break;
-            }
-            if (0 == n) {
-                usleep(10 * 1000);
-                continue;
-            }
-            if (EOF != b[0])
-                response += b[0]; 
-        } while (EOF != b[0]);
+        do {
+            byte = serial_recv(port);
+
+            cerr << "read " << byte << endl;
+
+            if (0 != byte)
+                response += (char)byte;
+        } while (0 != byte);
+        if (response.size() > 0)
+            response = response.substr(0, response.size()-1);
+        for (uint32_t i = 0; i < 1000; i++) {
+            usleep(1000);
+            serial_recv(port);
+        }
     } else {
         cerr << "Failed to read from \"" << portName << "\"" << endl;
     }
@@ -110,14 +86,21 @@ string XV25::receive(void)
 
 status_t XV25::command(string cmd)
 {
-    return send(cmd);
+    status_t ret = STATUS_OK;
+
+    if (STATUS_OK == send(cmd+"\n"))
+        receive();
+    else
+        ret = STATUS_ERROR;
+    
+    return ret;
 }
 
 status_t XV25::commandWithResponse(string cmd, string *response)
 {
     status_t ret = STATUS_OK;
 
-    if (STATUS_OK == send(cmd))
+    if (STATUS_OK == send(cmd+"\n"))
     	*response = receive();
     else
         ret = STATUS_ERROR;
@@ -132,13 +115,15 @@ status_t XV25::getVersion(string* version)
 
 status_t XV25::setTestMode(testMode_t testMode)
 {
+    string tmp;
     string cmd = "TestMode ";
     cmd += (testModeOn == testMode) ? "On" : "Off";
-    return command(cmd);
+    return commandWithResponse(cmd, &tmp);
 }
 
 status_t XV25::setMotor(motor_t motor, int speed, int distance)
 {
+    string tmp;
     status_t ret = STATUS_OK;;
     string cmd = "SetMotor ";
 
@@ -152,7 +137,7 @@ status_t XV25::setMotor(motor_t motor, int speed, int distance)
         ostringstream oss;
         oss << distance << " Speed " << speed;
         cmd += oss.str();
-        ret = command(cmd);
+        ret = commandWithResponse(cmd, &tmp);
     }
 
     return ret;
