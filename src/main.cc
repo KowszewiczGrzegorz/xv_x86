80e4582947e68c5a6d335b42db2d19ea0c07f149
lekeux <max.lekeux@gmail.com>
2012-11-26 12:12:46 +0100

    Préparation tests de communication à 921600bauds

--- src/main.cc
+++ src/main.cc
@@ -8,7 +8,6 @@ static const string portName = "/dev/ttyACM0";
 int main (void)
 {
     XV25 *xv25 = new XV25(portName);
-    PointsLibrary *pl = new PointsLibrary();
     string version;
 
     if (STATUS_ERROR == xv25->connect()) {
@@ -48,23 +47,22 @@ int main (void)
     xv25->setTestMode(testModeOn);
     usleep(1000);
 
-    ldsScan_t scan;
     xv25->startLDS();
     sleep(3);
 
+#if 0
     double lSpeed, rSpeed;
-    double dist90;
-    // double dist45;
+    ldsScan_t scan;
     int t = 0;;
     while (t < 1) {
         xv25->getLDSScan(&scan);
         
-        dist90 = xv25->getDistanceAtAngle(&scan, 90);
+        double dist90 = xv25->getDistanceAtAngle(&scan, 90);
         lSpeed = 150.0 + (400.0 - dist90)/100;
         rSpeed = 150.0 - (400.0 - dist90)/100;
 
         /*
-        dist45 = xv25->getDistanceAtAngle(45.0);
+        double dist45 = xv25->getDistanceAtAngle(45.0);
         if (dist45 < 400.0) {
             lSpeed += 400 - dist45;
             rSpeed -= 400 - dist45;
@@ -72,15 +70,18 @@ int main (void)
         */
 
         cerr << "dist = " << dist90 << " => cmd motor = [" << lSpeed << ", " << rSpeed << "]" << endl;
-        // xv25->setMotor(leftWheel, lSpeed, 10000);
-        // xv25->setMotor(rightWheel, rSpeed, 10000);
+        // xv25->setMotor(leftWheel, lSpeed, 100);
+        // xv25->setMotor(rightWheel, rSpeed, 100);
 
         usleep(10000);
         t++;
     }
+#endif
 
     xv25->stopLDS();
 
+    /*
+    PointsLibrary *pl = new PointsLibrary();
     FILE *fp = fopen("scan.csv", "w");
     vector<point_t> points;
     point_t p;
@@ -100,6 +101,7 @@ int main (void)
     vector<line_t> lines = pl->ropeAlgorithm(points);
     for (uint32_t i = 0; i < lines.size(); i++)
         cerr << "Line " << i << " = " << lines[i].a << "*x + " << lines[i].b << "*y + " << lines[i].c << endl;
+    */
 
     xv25->setTestMode(testModeOff);
 
