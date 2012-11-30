6d22bd9bd3893d9e19b0dfbe4803dcd2c317475b
lekeux <max.lekeux@gmail.com>
2012-11-30 09:28:44 +0100

    Ajout de passage à la ligne

--- web/webApiTest.php
+++ web/webApiTest.php
@@ -16,34 +16,34 @@ $address = gethostbyname('127.0.0.1');
 /* Crée un socket TCP/IP. */
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 if ($socket === false) {
-    echo "socket_create() a échoué : raison :  " . socket_strerror(socket_last_error()) . "\n";
+    echo "socket_create() a échoué : raison :  " . socket_strerror(socket_last_error()) . "<br/>\n";
 } else {
-    echo "OK.\n";
+    echo "OK.<br/>\n";
 }
 
-echo "Essai de connexion à '$address' sur le port '$service_port'...";
+echo "Essai de connexion à '$address' sur le port '$service_port'...<br/>";
 $result = socket_connect($socket, $address, $service_port);
 if ($socket === false) {
-    echo "socket_connect() a échoué : raison : ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
+    echo "socket_connect() a échoué : raison : ($result) " . socket_strerror(socket_last_error($socket)) . "<br/>\n";
 } else {
-    echo "OK.\n";
+    echo "OK.<br/>\n";
 }
 
 $in = "GetVersion\n";
 
-echo "Envoi de la requête '$in'\n";
+echo "Envoi de la requête '$in'<br/>\n";
 socket_write($socket, $in, strlen($in));
-echo "OK.\n";
+echo "OK.<br/>\n";
 
-echo "Lire la réponse : \n";
+echo "Lire la réponse : <br/>\n";
 $out = '';
 while ($out = socket_read($socket, 2048)) {
     echo $out;
 }
 
-echo "Fermeture du socket...";
+echo "Fermeture du socket...<br/>";
 socket_close($socket);
-echo "OK.\n\n";
+echo "OK.<br/>\n\n";
 ?>
 </body>
 </html>
