132eed5b020fed595b2d86aa7b71df53f4555235
Max <max@portable.(none)>
2012-12-01 16:51:07 +0100

    On remplis tout ça

--- web/webApiForm.php
+++ web/webApiForm.php
@@ -1,27 +1,47 @@
 <html>
-<head><title>Web API Test</title></head>
-<style type="text/css">
-p.form {
-    order:1px solid #A9A9A9; 
-    padding:10px 20px 10px 20px;
-    margin:10px;
-    text-align:center;
-    background-color:#D3D3D3;
-}
-</style>
-<body>
-<h2>Web API Test</h2>
+<head>
+    <title>Web interface for Neato XV-25</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script language="javascript" type="text/javascript">
+        nbCommandsInHistory = 2;
+        var commands = new Array();
+<?php
 
-<p class="form">
-    <form action="webAPIForm.php" method="post">
-       Commande : <input type="cmd" name="nom" /> <input type="submit" value="OK">
+?>
+
+    function showMoreHistory(nb) {
+        i = 0;
+        while (i < nb && nbCommandsInHistory < commands.length) {
+            document.getElementById('historyList').innerHTML += "    <p class=\"history\"><a href=\"#\" onclick=\"document.getElementById('cmd').value='" 
+                                                                + commands[nbCommandsInHistory] + "';return false;\">" 
+                                                                + commands[nbCommandsInHistory] + "</a></p>\n";
+	    nbCommandsInHistory++;
+            i++;
+        }
+        if ( nbCommandsInHistory >= commands.length) {
+            document.getElementById('moreButtonDiv').innerHTML = "";
+        }
+    };
+</script>
+</head>
+<body>
+<h1>Web interface for Neato XV-25</h1>
+
+<img class="logo" src="xv-25.png" alt="neato XV-25" />
+
+<div class="form">
+    <h2>Commande</h2>
+    <form id="form_id" action="webAPIForm.php" method="post">
+        <input type="cmd" id="cmd" name="nom" maxlength="30" size="30" /> <input type="submit" value="OK">
     </form>
-</p>
-     
+
 <?php
 error_reporting(E_ALL);
 
 if (isset($_POST['cmd'])) {
+
+    echo "<div class=\"response\">\n";
+
     $service_port = 8112;
     $address = gethostbyname('127.0.0.1');
 
@@ -61,7 +81,36 @@ if (isset($_POST['cmd'])) {
     echo "Fermeture du socket...<br/>";
     socket_close($socket);
     echo "OK.<br/>\n\n";
+    echo "</div>\n";
 }
+?>
+</div>
+
+<?php
+if (isset($_POST['response'])) {
+    echo "<div class="form">\n";
+    echo "<h2>" . htmlspecialchars($_POST['cmd_response']) . "</h2>\n";
+    echo "<p class=\"response\">\n";
+    echo "" . str_replace("\n", "<br/>\n", htmlspecialchars($_POST['response'])) . "\n";
+    echo "</p>\n":
+    echo "</div>\n";
+}
+?>
+
+<?php
+if (isset($_POST['history'])) {
+    echo "<div class=\"form\">\n";
+    echo "<h2>Historique</h2>\n";
+    echo "<div id=\"historyList\">\n";
+    echo "    <p class=\"history\"><a href=\"#\" onclick=\"document.getElementById('cmd').value='GetVersion';return false;\">GetVersion</a></p>\n";
+    echo "    <p class=\"history\"><a href=\"#\" onclick=\"document.getElementById('cmd').value='SetMotor';return false;\">SetMotor</a></p>\n";
+    echo "</div>\n";
+    echo "<div class=\"centered\" id=\"moreButtonDiv\"><div id=\"moreButton\"><a href=\"javascript:showMoreHistory(2)\"/><< more >></a></div></div>\n";
+    echo "</div>\n";
+}     
+
+<?php
+
 ?>
 </body>
 </html>
